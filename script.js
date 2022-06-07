576064760e67b1817132dda6c33a08dca8f1f07e
lucaval95 <84185641+lucaval95@users.noreply.github.com>
2022-06-07 11:58:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 (function ($) {
   
   $('#bottoneprova').click(function () {
-    window.postMessage('message',  '*');
+    window.parent.postMessage('message',  '*');
   });
   
   // We use some Javascript and the URL #fragment to hide/show different parts of the page
